965999fd9448f369305188d060431a51f9bc1c77
Priyanshu055 <pm9581543@gmail.com>
2025-08-10 20:46:26 +0530

    869

---
+++ Medium/leetcode869.cpp
@@ -0,0 +1,20 @@
+// convert each no into string then short it now find the power of two for each term like 0,1,2,3,.... now sort that particular power of two if they get same return true else false;
+
+class Solution {
+public:
+     
+    string getsorted(int n){
+        string s=to_string(n);
+        return s;
+    }
+
+    bool reorderedPowerOf2(int n) {
+        string s =getsorted(n);
+        for(int i=0;i<=29;i++){
+            if(s==getsorted(1<<i)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
