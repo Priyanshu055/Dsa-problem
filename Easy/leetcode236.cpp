b710d466b00475b56cd8904f406013bb4a094f0c
Priyanshu055 <pm9581543@gmail.com>
2025-08-13 21:44:40 +0530

    236

---
+++ Easy/leetcode236.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool solve(int n){
+        if(n<=0) return false;
+        if(n==1) return true;
+        if(n%3!=0) return false;
+        return solve(n/3);
+    }
+    bool isPowerOfThree(int n) {
+        return solve(n);
+    }
+};
