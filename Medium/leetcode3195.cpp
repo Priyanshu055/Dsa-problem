ec713830047e9605a6e950557771860e1f735047
Priyanshu055 <pm9581543@gmail.com>
2025-08-22 21:04:20 +0530

    3195

---
+++ Medium/leetcode3195.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumArea(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+
+        int minrow=m;
+        int maxrow=-1,mincol=n,maxcol=-1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    minrow=min(minrow,i);
+                    maxrow=max(maxrow,i);
+
+                    mincol=min(mincol,j);
+                    maxcol=max(maxcol,j);
+
+                }
+            }
+          
+        }
+          return (maxrow-minrow+1)*(maxcol-mincol+1);
+    }
+};
